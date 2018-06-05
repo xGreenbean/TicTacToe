01951f79893bd38b3470f9376c3263f7a2c717a6
ehud <ehud.plaksin@gmail.com>
2018-06-05 15:35:44 +0300

    new project, still have to add comments and orginize

--- Board.cpp
+++ Board.cpp
@@ -47,6 +47,7 @@ Board& Board::operator=( char c) {
  * @return
  */
 Board& Board::operator=(const Board& b) {
+    this->~Board();
     m_size=b.m_size;
     this->m_board=new Piece*[m_size];
     for (int i = 0; i < m_size; ++i) {
